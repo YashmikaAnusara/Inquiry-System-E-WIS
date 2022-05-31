f343c0c73db9c2a7d687b9c6da811584bf96e036
Yashmika Anusara <yashmikaaunusara@gmail.com>
2022-06-01 00:44:46 +0530

    add Login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminHome from "./pages/AdminHome";
 import InquiryForm from "./pages/InquiryForm";
+import Login from "./pages/LoginPage";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-
           {/* <Route path="/" element={<AdminHome />}/> */}
-          <Route path="/dashBoard" element={<AdminHome />}/>
+          <Route path="/dashBoard" element={<AdminHome />} />
 
-          <Route path="/" element={<AdminHome />}/>
-          <Route path="/InquiryForm" element={<InquiryForm />}/>
-
+          <Route path="/" element={<AdminHome />} />
+          <Route path="/InquiryForm" element={<InquiryForm />} />
+          <Route path="/Login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
