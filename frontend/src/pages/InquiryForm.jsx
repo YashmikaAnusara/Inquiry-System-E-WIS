8b53be5e45a0d55c411154706d23518335f419d6
Yashmika Anusara <yashmikaaunusara@gmail.com>
2022-05-31 12:42:50 +0530

    Inquiry Form

---
+++ frontend/src/pages/InquiryForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../css/InquiryForm.css'
+
+export default function InquiryForm() {
+  return (
+    <div>
+      <h1 className='Inquiryheading'>Inquiry Form</h1>
+    </div>
+  )
+}
