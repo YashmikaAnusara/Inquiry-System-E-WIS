ff7451ba7d58a91b8296c89e8f643159abcbbf43
Thivanka Saparamadu <86546750+thiva99@users.noreply.github.com>
2022-06-07 09:59:49 +0530

    updated

--- frontend/src/components/ManagerNavBar.jsx
+++ frontend/src/components/ManagerNavBar.jsx
@@ -6,7 +6,7 @@ import PeopleIcon from "@mui/icons-material/People";
 import { Link } from "react-router-dom";
 import BranchReg from "./BranchRegForm";
 import BranchMenu from "./BranchMenu";
-
+import Pic from '../images/pic5.png'
 
 function ManagerNavBar() {
   const [active] = useState(false);
@@ -15,7 +15,9 @@ function ManagerNavBar() {
       {active ? <BranchReg /> : ""}
 
       <div className="headerWraper">
-        <div className="logo-wrapper"></div>
+      <div className='logo-wrapper'>
+                    <div className='company-logo'><img src={Pic} alt="logo" className='company-image'/></div>
+                </div>
         <div className="menu-wrapper">
           <div className="menu-icon">
             <BranchMenu />
